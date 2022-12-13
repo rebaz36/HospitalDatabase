1c24fd705f6cd32ffa65f49c8fc9a91ac7b21194
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-12-13 16:31:41 -0500

    Add the foreign keys

--- Diagram_Schema.sql
+++ Diagram_Schema.sql
@@ -48,6 +48,32 @@ CREATE TABLE treatments_history (
     PRIMARY KEY(id)
 );
 
+-- Foreign keys
+ALTER TABLE medical_histories
+ADD CONSTRAINT fk_medical_histories
+FOREIGN KEY (patient_id)
+REFERENCES patients (id);
+ALTER TABLE invoices
+ADD CONSTRAINT fk_invoices
+FOREIGN KEY (medical_history_id)
+REFERENCES medical_histories (id);
+ALTER TABLE invoice_items
+ADD CONSTRAINT fk_invoices_items
+FOREIGN KEY (invoice_id)
+REFERENCES invoices (id);
+ALTER TABLE invoice_items
+ADD CONSTRAINT fk_invoice_treatments
+FOREIGN KEY (treatment_id)
+REFERENCES treatments (id);
+ALTER TABLE treatments_history
+ADD CONSTRAINT fk_treatments_history
+FOREIGN KEY (medical_history_id)
+REFERENCES medical_histories (id);
+ALTER TABLE treatments_history
+ADD CONSTRAINT fk_treatments
+FOREIGN KEY (treatment_id)
+REFERENCES treatments (id);
+
 -- Indexes for Foreign Keys
 CREATE INDEX ON medical_histories (patient_id);
 CREATE INDEX ON invoices (medical_history_id);
